4dcbcac18a0df8011febb34e43d5beb5a444b779
gargibisht <bisht.gargi19@gmail.com>
2021-09-19 15:13:03 +0530

    books_authors_publications_apis_done

--- requirements.js
+++ requirements.js
@@ -15,20 +15,20 @@
 //What are all the APIs which we need.....
 
 //Books
-//We need an API        done
+//We need an API        
 //To get all the books      done
 //To get specific books     done
 //To get list of books based on category        done
 //To get list of books based on languages       done
 
 //Authors
-//We need an API        done
+//We need an API        
 //To get all the authors        done
 //To get specific authors by id number       done
 //To get list of authors based on books ISBN        done
 
 //Publications
 //We need an API
-//To get all the publications
-//To get specific publications
-//To get list of publications based on books
+//To get all the publications       done
+//To get specific publications      done
+//To get list of publications based on books           done

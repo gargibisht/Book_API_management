056572e0087dc65b618a1d869dd488636ebe24ed
gargibisht <bisht.gargi19@gmail.com>
2021-10-05 04:21:36 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
+
+const mongoose = require("mongoose");
 
 //Database
 const database = require("./database");
@@ -8,6 +12,17 @@ const booky = express();
 
 //configuration
 booky.use(express.json());
+
+//establish database connection
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        /*useFindAndModify: false,
+        useCreateIndex: true,*/
+    })
+    .then(() => console.log("Connection Established!!!!!!!!!!!"));
+
 
 booky.listen(3000, () => console.log("Hey! server is running!!")); /*in the terminal it shows whether our localhost:3000 is working or not.*/
 
